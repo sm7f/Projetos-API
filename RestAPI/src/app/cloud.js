024db79bdfcacd6bd0596c64ae96f61986994b0e
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-01-21 00:49:01 -0300

    .

--- RestAPI/src/app/cloud.js
+++
@@ -1,9 +0,0 @@
-import {Cloudinary} from "@cloudinary/url-gen";
-
-const Cloud = () => {
-  const cld = new Cloudinary({cloud: {cloudName: 'dnf64htdf'}});
-};
-
-cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg.",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
